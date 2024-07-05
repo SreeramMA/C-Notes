9730524eb62709e43d8a20cc5cfddc06a223e5e5
SreeramMA <85754403+SreeramMA@users.noreply.github.com>
2024-07-05 12:45:45 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int var1=5;
+    printf("%d", var1); //Use double quotes for %d
+    return 0;
+
+}
+// Naming Rules:
+// Do not use integer or underscore in the beginning of the variable
+// Do not use other symbols
